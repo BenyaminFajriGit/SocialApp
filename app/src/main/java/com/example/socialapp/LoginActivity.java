c961770e02b50f32f3b71f6103e923e964cf15bb
frozenbits <47202084+frozenbits@users.noreply.github.com>
2019-11-03 23:24:27 +0700

    progress on fixing login problems

--- app/src/main/java/com/example/socialapp/LoginActivity.java
+++ app/src/main/java/com/example/socialapp/LoginActivity.java
@@ -7,6 +7,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -59,6 +60,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
+                Log.d("onpostexec",s);
                 loading.dismiss();
                 isSuccess(s);
             }
@@ -71,22 +73,28 @@ public class LoginActivity extends AppCompatActivity {
 
                 RequestHandler rh = new RequestHandler();
                 String res = rh.sendPostRequest("http://frozenbits.tech/socialAppWS/DataUser/validateLogin", params);
+                //Log.d("res",res); //Get fetch result
                 String dataPull = "";
                 try{
                     JSONObject result = new JSONObject(res);
-                    JSONArray userData = result.getJSONArray("data");
-                    String id_user = userData.getString(0);
+                    JSONArray rawUserData = result.getJSONArray("data");
+                    String rud = result.getString("data");
+                    //Log.d("rawuserdata",rud); //get "data" key from json object
+                    JSONObject userData = rawUserData.getJSONObject(0); //create inner json object
+                    String id_user = userData.getString("id_user");
+                    //Log.d("iduser",id_user); //get "id_user" key from inner json object
 
                     if (result.getBoolean("status")){
                         rh = new RequestHandler();
                         params = new HashMap<>();
                         params.put("id_user", id_user);
+
                         dataPull = rh.sendPostRequest("http://frozenbits.tech/socialAppWS/DataUser/getDataUser",params);
                     }
                 } catch (JSONException e){
                     e.printStackTrace();
                 }
-                Log.d("datapull", dataPull);
+                Log.d("datapull",dataPull);
                 return dataPull;
                 //OK hasil query udah bener, tinggal benerin ini kenapa error di bagian isSuccess()
                 //cek lagi nanti, gue pengen tidur.
@@ -99,6 +107,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private void isSuccess(String json){
         try {
+            Log.d("something", json);
             JSONObject jsonObject = new JSONObject(json);
             boolean status= jsonObject.getBoolean("status");
             String message= jsonObject.getString("message");
